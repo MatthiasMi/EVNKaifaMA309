dd1082209d181c381325067e1389d2a31bd0db1b
&mut hias <&mut@hi.as>
2024-01-05 11:20:29 +0100

    [:zap:] :mute: Add Smartmeter Logs

--- smartmeter.py
+++ smartmeter.py
@@ -29,24 +29,76 @@ comport = "/dev/ttyUSB0" # Config/Init
 
 ser = serial.Serial(port=comport, baudrate=2400, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE)
 
-curtime = str(datetime.now())
-if o: print(f"Hello EVN - reading data (~3 seconds) on{curtime}")
+#Comport Config/Init
+comport = "/dev/ttyUSB0"
 
-data = ser.read(size=282).hex()
-if o: print("Hello EVN - data read.\n\ndata:\n=====")
-if o: print(data)
+tries=0
+def hexstr2int(uints, index, width=4):
+    """ `hexstr2int` converts hex string `uints` starting at `index` with `width` to `int`."""
+    return int(str(uints)[index:index+width],16)
 
-msg = GXDLMSTranslatorMessage()
-msg.message = GXByteBuffer(data)
-gxbb = GXByteBuffer()
-gxdlmstr.completePdu = True
-markup = ""
-while gxdlmstr.findNextFrame(msg, gxbb):
-    gxbb.clear()
-    markup += gxdlmstr.messageToXml(msg)
+while 1:
+    timestamp = str(datetime.now())
+    if o: print(f"Hello EVN - reading data (~3 seconds) on{timestamp}")
+    
+    data = ser.read(size=282).hex()
+    if o: print("Hello EVN - data read.\n\ndata:\n=====")
+    if o: print(data)
 
-bs = BeautifulSoup(markup, "html.parser")
+    msg = GXDLMSTranslatorMessage()
+    msg.message = GXByteBuffer(data)
+    gxbb = GXByteBuffer()
+    gxdlmstr.completePdu = True
+    markup = ""
+    while gxdlmstr.findNextFrame(msg, gxbb):
+        gxbb.clear()
+        markup += gxdlmstr.messageToXml(msg)
 
-uints32 = str(bs.find_all("uint32"))
-uints16 = str(bs.find_all("uint16"))
-if o: print("uints16", uints16); print("uints32", uints32)
+    bs = BeautifulSoup(markup, "html.parser")
+
+    uints32 = str(bs.find_all("uint32"))
+    uints16 = str(bs.find_all("uint16"))
+    if o: print("uints16", uints16); print("uints32", uints32)
+
+    width=4 # uints16
+    # Voltage L 1,2,3 in [V]
+    voltageL1 = hexstr2int(uints16, 16, width)/10
+    voltageL2 = hexstr2int(uints16, 48, width)/10
+    voltageL3 = hexstr2int(uints16, 80, width)/10
+
+    # Vurrent L 1,2,3 in [A]
+    currentL1 = hexstr2int(uints16, 112, width)/100
+    currentL2 = hexstr2int(uints16, 144, width)/100
+    currentL3 = hexstr2int(uints16, 176, width)/100
+
+    factor = hexstr2int(uints16, 208, width)/1000
+    
+    width*=2 # uints32
+    # Energy A+/- in [Wh] -> [kWh]
+    energyP = hexstr2int(uints32, 16,width)/1000
+    energyN = hexstr2int(uints32, 52, width)/1000
+
+    # Power P+/- in [W]
+    powerP = hexstr2int(uints32, 88, width)
+    powerN = hexstr2int(uints32, 124, width)
+    powerD = powerP-powerN
+
+    s = ""
+    st = f"power  : {powerD:.2f} [W],\n"; s+=st
+    if o: print(st)
+    st = f"consume: {energyP:.2f} [KWh],\n"; s+=st
+    if o: print(st)
+    st = f"supply : {energyN:.2f} [KWh],\n"; s+=st
+    if o: print(st)
+    st = f"L1 : {voltageL1:.2f} [V], {currentL1:.2f} [A],\n"; s+=st
+    if o: print(st)
+    st = f"L2 : {voltageL2:.2f} [V], {currentL2:.2f} [A],\n"; s+=st
+    if o: print(st)
+    st = f"L3 : {voltageL3:.2f} [V], {currentL3:.2f} [A].\n"; s+=st
+    if o: print(st)
+    st = f"powerN : {powerN:.2f} [W],\n"; s+=st
+    if o: print(st)
+    st = f"powerD : {powerD:.2f} [W],\n"; s+=st
+    if o: print(st)
+    st = f"data timestamp: {timestamp}"; s+=st
+    if o: print(st)
